0a6d5b5713446db12d115036cc383e13725b7a6f
Mikhail_Aris <krasnoperov_mi@ekb.sima-land.ru>
2022-12-02 11:08:24 +0500

    day 2

--- day02/day02.go
+++ day02/day02.go
@@ -14,6 +14,50 @@ func DayTwo() error {
 	if err != nil {
 		return err
 	}
+	partOne(data)
+	patTwo(data)
+
+	return nil
+}
+func partOne(data []string) {
+	var points int
+	var sum int
+	for _, v := range data {
+		if string(v[0]) == "A" {
+			switch {
+			case string(v[2]) == "X":
+				points = 1 + 3
+			case string(v[2]) == "Y":
+				points = 2 + 6
+			case string(v[2]) == "Z":
+				points = 3 + 0
+			}
+		}
+		if string(v[0]) == "B" {
+			switch {
+			case string(v[2]) == "X":
+				points = 1 + 0
+			case string(v[2]) == "Y":
+				points = 2 + 3
+			case string(v[2]) == "Z":
+				points = 3 + 6
+			}
+		}
+		if string(v[0]) == "C" {
+			switch {
+			case string(v[2]) == "X":
+				points = 1 + 6
+			case string(v[2]) == "Y":
+				points = 2 + 0
+			case string(v[2]) == "Z":
+				points = 3 + 3
+			}
+		}
+		sum += points
+	}
+	fmt.Println("part one", sum)
+}
+func patTwo(data []string) {
 	var points int
 	var sum int
 	for _, v := range data {
@@ -49,7 +93,5 @@ func DayTwo() error {
 		}
 		sum += points
 	}
-	fmt.Println(sum)
-
-	return nil
+	fmt.Println("part two", sum)
 }
